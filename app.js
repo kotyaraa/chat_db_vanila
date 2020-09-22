5f89f1d9619ef460ae1a8407eac5441933d01c0d
kotyaraa <42804223+kotyaraa@users.noreply.github.com>
2020-09-22 21:41:38 +0300

    added fetch and form

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("hello FATCH!!!")
+console.log("hello FETCH!!!");
+
+fetch('./db.js')
+.then((response)=>{
+    return response.json()
+})
+.then(data => {
+    const div = document.querySelector(".chat");
+    const txtChat = document.createElement("p");
+    txtChat.innerHTML = data[0].name
+    div.appendChild(txtChat);
+});
